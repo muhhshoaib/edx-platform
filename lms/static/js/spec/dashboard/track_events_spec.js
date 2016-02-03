3c18fa2c50321344428e9f4f90b792c8af2cd5df
Simon Chen <schen@edx.org>
2016-02-03 16:56:05 +0000

    ECOM-3177 fix flaky js tests on dashboard.TrackEvents

--- lms/static/js/spec/dashboard/track_events_spec.js
+++ lms/static/js/spec/dashboard/track_events_spec.js
@@ -14,70 +14,39 @@
                 window.edx.dashboard.trackEvents();
             });
 
-            it('sends an analytics event when the user clicks course title link', function() {
-                // Verify that analytics events fire when the 'course title link' is clicked.
-                expect(window.analytics.trackLink).toHaveBeenCalledWith(
-                    $('.course-title > a'),
+            it('sends analytics events when the user interacts with dashboard', function() {
+                expect(window.analytics.trackLink.callCount).toBe(7);
+
+                var expectedArray = [
+                    [$('.course-title > a'),
                     'edx.bi.dashboard.course_title.clicked',
-                     window.edx.dashboard.generateTrackProperties
-                );
-            });
-
-            it('sends an analytics event when the user clicks course image link', function() {
-                // Verify that analytics events fire when the 'course image link' is clicked.
-                expect(window.analytics.trackLink).toHaveBeenCalledWith(
-                    $('.cover'),
+                    window.edx.dashboard.generateTrackProperties],
+                    [$('.cover'),
                     'edx.bi.dashboard.course_image.clicked',
-                    window.edx.dashboard.generateTrackProperties
-                );
-            });
-
-
-            it('sends an analytics event when the user clicks enter course link', function() {
-                // Verify that analytics events fire when the 'enter course link' is clicked.
-                expect(window.analytics.trackLink).toHaveBeenCalledWith(
-                    $('.enter-course'),
+                    window.edx.dashboard.generateTrackProperties],
+                    [$('.enter-course'),
                     'edx.bi.dashboard.enter_course.clicked',
-                    window.edx.dashboard.generateTrackProperties
-                );
-            });
-
-            it('sends an analytics event when the user clicks enter course link', function() {
-                // Verify that analytics events fire when the options dropdown is engaged.
-                expect(window.analytics.trackLink).toHaveBeenCalledWith(
-                    $('.wrapper-action-more'),
+                    window.edx.dashboard.generateTrackProperties],
+                    [$('.wrapper-action-more'),
                     'edx.bi.dashboard.course_options_dropdown.clicked',
-                    window.edx.dashboard.generateTrackProperties
-                );
-            });
-
-            it('sends an analytics event when the user clicks the learned about verified track link', function() {
-                //Verify that analytics events fire when the 'Learned about verified track' link is clicked.
-                expect(window.analytics.trackLink).toHaveBeenCalledWith(
-                    $('.verified-info'),
+                    window.edx.dashboard.generateTrackProperties],
+                    [$('.verified-info'),
                     'edx.bi.dashboard.verified_info_link.clicked',
-                    window.edx.dashboard.generateTrackProperties
-                );
-            });
-
-            it('sends an analytics event when the user clicks find courses button', function() {
-                // Verify that analytics events fire when the 'user clicks find the course' button.
-                expect(window.analytics.trackLink).toHaveBeenCalledWith(
-                    $('.btn-find-courses'),
+                    window.edx.dashboard.generateTrackProperties],
+                    [$('.btn-find-courses'),
                     'edx.bi.dashboard.find_courses_button.clicked',
                     {
                         category: 'dashboard',
                         label: null
-                    }
-                );
-            });
-
-            it('sends an analytics event when the user clicks the \'View XSeries Details\' button', function() {
-                expect(window.analytics.trackLink).toHaveBeenCalledWith(
-                    $('.xseries-action .btn'),
+                    }],
+                    [$('.xseries-action .btn'),
                     'edx.bi.dashboard.xseries_cta_message.clicked',
-                    window.edx.dashboard.generateProgramProperties
-                );
+                    window.edx.dashboard.generateProgramProperties]
+                ];
+
+                for (var cnt=0; cnt< expectedArray.length; cnt ++){
+                    expect(window.analytics.trackLink.argsForCall[cnt]).toEqual(expectedArray[cnt]);
+                };
             });
 
             it('sends an analytics event when xseries messages are present in the DOM on page load', function() {
